18f954ea74aedaf4b074be456dee236eb69c3e49
Daniel Sogl <daniel@finanzen-zero.net>
2022-12-16 22:04:21 +0100

    add capacitor config

---
+++ android/app/src/main/java/de/danielsogl/webdavedemo/MainActivity.java
@@ -0,0 +1,5 @@
+package de.danielsogl.webdavedemo;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'de.danielsogl.webdavedemo',
+  appName: 'WebDaveDemo',
+  webDir: 'www',
+  bundledWebRuntime: false,
+};
+
+export default config;
